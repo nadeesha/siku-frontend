a991b3e867171d114552387d44ac00ffd1cff2a1
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2017-06-07 23:34:19 +1000

    With Semantic UI

---
+++ src/components/common/Header/Header.js
@@ -0,0 +1,45 @@
+// @flow
+
+import React from 'react';
+import { Menu } from 'semantic-ui-react';
+import { withState } from 'recompose';
+
+const Header = ({
+  activeItem,
+  setActiveItem,
+}: {
+  activeItem: 'home' | 'messages' | 'friends' | 'logout',
+  setActiveItem: (item: string) => void,
+}): React.Element<*> => (
+  <div>
+    <Menu borderless>
+      <Menu.Item header>thisday.io</Menu.Item>
+      <Menu.Item
+        name="home"
+        active={activeItem === 'home'}
+        onClick={(): void => setActiveItem('home')}
+      />
+      <Menu.Item
+        name="messages"
+        active={activeItem === 'messages'}
+        onClick={(): void => setActiveItem('messages')}
+      />
+      <Menu.Item
+        name="friends"
+        active={activeItem === 'friends'}
+        onClick={(): void => setActiveItem('friends')}
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="logout"
+          active={activeItem === 'logout'}
+          onClick={(): void => setActiveItem('logout')}
+        />
+      </Menu.Menu>
+    </Menu>
+  </div>
+);
+
+const enhance = withState('activeItem', 'setActiveItem', 'home');
+
+export default enhance(Header);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 const rootEl = document.getElementById('root');
